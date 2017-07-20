2b606c880b384ddd607ec19091aca5caa5d4f7ce
Chad Mitchell <chad39@gmail.com>
2017-07-20 13:45:18 -0600

    General update and code cleanup

--- tweet_scrubber.py
+++ tweet_scrubber.py
@@ -10,7 +10,10 @@ functions in module:
 - scrub_tweet: runs all other cleaning functions as a general tweet clean.
 """
 
+# Imports
 import re
+
+# Stop words list
 stop_words = "i me my myself we our ours ourselves you your yours yourself yourselves he him his himself she her hers herself it its itself they them their theirs themselves what which who whom this that these those am is are was were be been being have has had having do does did doing a an the and but if or because as until while of at by for with about against between into through during before after above below to from up down in out on off over under again further then once here there when where why how all any both each few more most other some such no nor not only own same so than too very s t can will just don should now".split()
 
 
@@ -33,12 +36,7 @@ def remove_hashtags(tweet):
 
 
 def remove_stopwords(tweet):
-    tweet_split = tweet.split()
-    t = tweet.split()
-    for word in tweet_split:
-        if word in stop_words:
-            t.remove(word)
-    t = " ".join(t)
+    t = "".join([ for w in tweet.split() if w not in stop_words])
     return t
 
 
